21219c4f0d8836965143553e50117e0548aa165f
Arb Inc Dev <dev@arbinc.io>
2026-03-22 03:17:04 +0100

    feat: add OG/Twitter meta tags for link previews

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,8 +3,29 @@ import StyledComponentsRegistry from '../lib/registry'
 import { Analytics } from '@vercel/analytics/next'
 
 export const metadata = {
-  title: 'Swap Widget',
-  description: 'KyberSwap Widget',
+  title: 'Arbitrage Inc | Swap Widget',
+  description: 'Swap tokens with the best rates using KyberSwap. Powered by Arbitrage Inc.',
+  openGraph: {
+    title: 'Arbitrage Inc | Swap Widget',
+    description: 'Swap tokens with the best rates using KyberSwap. Powered by Arbitrage Inc.',
+    type: 'website',
+    url: 'https://arbitrage-inc.exchange/',
+    siteName: 'Arbitrage Inc',
+    images: [
+      {
+        url: '/og-image.svg',
+        width: 1200,
+        height: 630,
+        alt: 'Arbitrage Inc Swap Widget',
+      },
+    ],
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Arbitrage Inc | Swap Widget',
+    description: 'Swap tokens with the best rates using KyberSwap. Powered by Arbitrage Inc.',
+    images: ['/og-image.svg'],
+  },
 }
 
 export default function RootLayout({
