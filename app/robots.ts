1af9cacd509422f305668bd431b58bfdf518a133
Lukecele <87429074+Lukecele@users.noreply.github.com>
2026-04-02 19:22:22 +0200

    SEO: improve robots.txt with per-bot rules and host directive

--- app/robots.ts
+++ app/robots.ts
@@ -2,11 +2,19 @@ import { MetadataRoute } from 'next'
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: '*',
-      allow: '/',
-      disallow: '/api/',
-    },
+    rules: [
+      {
+        userAgent: '*',
+        allow: '/',
+        disallow: ['/api/', '/_next/', '/static/'],
+      },
+      {
+        userAgent: 'Googlebot',
+        allow: '/',
+        disallow: ['/api/'],
+      },
+    ],
     sitemap: 'https://arbitrage-inc.exchange/sitemap.xml',
+    host: 'https://arbitrage-inc.exchange',
   }
 }
