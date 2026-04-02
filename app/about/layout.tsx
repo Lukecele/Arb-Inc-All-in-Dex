aca9a9459eff599a500d33fa8fe32271d402db48
Lukecele <87429074+Lukecele@users.noreply.github.com>
2026-04-02 19:34:52 +0200

    SEO: add breadcrumb schema to about layout

--- app/about/layout.tsx
+++ app/about/layout.tsx
@@ -1,3 +1,17 @@
+const breadcrumb = {
+  '@context': 'https://schema.org',
+  '@type': 'BreadcrumbList',
+  itemListElement: [
+    { '@type': 'ListItem', position: 1, name: 'Home', item: 'https://arbitrage-inc.exchange/' },
+    { '@type': 'ListItem', position: 2, name: 'About', item: 'https://arbitrage-inc.exchange/about' },
+  ],
+}
+
 export default function AboutLayout({ children }: { children: React.ReactNode }) {
-  return <>{children}</>;
+  return (
+    <>
+      <script type="application/ld+json" dangerouslySetInnerHTML={{ __html: JSON.stringify(breadcrumb) }} />
+      {children}
+    </>
+  )
 }
